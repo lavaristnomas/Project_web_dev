067ff6a5e719c9c87495f88068869175a48f20a8
Brandon <bnowlin31@gmail.com>
2024-03-06 06:51:31 +0100

    updated for webhosting

--- Project/database.php
+++ Project/database.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$dbname = "login";
-$username = "root";
-$password = "";
+$dbname = "vdflakyl_TGH";
+$username = "vdflakyl_TGH";
+$password = "Hn3fKu4cDuktdSLU7B5B";
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
